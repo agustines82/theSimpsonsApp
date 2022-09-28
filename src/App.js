fec4380cc09906197fe1c247cc13de608aebef18
Agustin Sampayo <agustines82@gmail.com>
2022-09-28 18:30:58 -0300

    agregamos un componente condicional

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Spinner from "./components/Spinner";
 function App() {
     //creamos una variable estate para renderizar la frase de los simpsons
     const [personaje, setPersonaje] = useState({});
+    const [mostrarSpinner, setMostrarSpinner] = useState(true);
     //ciclo de vida
     //useEffect(()=>{}) esto se ejecuta en montaje y actualizacion
     //useEffect(()=>{},[]) esto se ejecuta solo en montaje
@@ -23,30 +24,44 @@ function App() {
         //como esto puede fallar se lo guarda en una estructura trychatch donde 1ro intentamos el codigo y si esta bien lo ejecuta y si no esta bien ejecuta el codigo de abajo. sirve para controlar errores
 
         try {
+            setMostrarSpinner(true);
             const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
             const dato = await respuesta.json();
             setPersonaje(dato[0]);
+
+            //si quiero retrasar la respuesta y que el sinner se muestre por mas tiempo: (no es necesario el setTimeout...)
+            setTimeout(() => {
+                setMostrarSpinner(false);
+            }, 2500);
+            //actualizar el state del spinner
         } catch (error) {
             //mostrar un cartel al usurario
+            setMostrarSpinner(false);
         }
     };
 
+    //operador ternario
+    //(consicion logica)? (codigo cuando es true) : (codigo cuando es false)
+
+    const mostrarComponente = mostrarSpinner ? <Spinner /> : <Frases personaje={personaje} />;
+
     return (
         <>
-            <section className="app">
-                <article className="d-flex flex-column align-items-center">
-                    <aside className="mt-3">
-                        <img className="mediaApp" src={theSimpsonBanner} alt="banner"></img>
-                    </aside>
-                    <aside className="mt-5">
-                        <Button type="button" variant="warning" onClick={consultarAPI}>
-                            Obtener frase
-                        </Button>
-                    </aside>
-                    <Spinner />
-                    <Frases personaje={personaje} />
-                </article>
-            </section>
+            <main className="app">
+                <section>
+                    <article className="d-flex flex-column align-items-center">
+                        <aside className="mt-3">
+                            <img className="mediaApp" src={theSimpsonBanner} alt="banner"></img>
+                        </aside>
+                        <aside className="mt-5">
+                            <Button type="button" variant="warning" onClick={consultarAPI}>
+                                Obtener frase
+                            </Button>
+                        </aside>
+                        {mostrarComponente}
+                    </article>
+                </section>
+            </main>
         </>
     );
 }
